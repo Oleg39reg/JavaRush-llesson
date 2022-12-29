0ad8c641e1a4c75aac2b4237391e820fff588626
Oleg <olegkuz39@gmail.com>
2022-12-29 17:54:34 +0200

    Лекция 1 JavaRush пример1:

---
+++ src/Hous.java
@@ -0,0 +1,12 @@
+public class Hous {
+    int a;
+    int b;
+    public static void main (String[] arge)
+    {
+        System.out.print(1);
+    }
+    public static double pi ()
+    {
+        return 3.14;
+    }
+}
