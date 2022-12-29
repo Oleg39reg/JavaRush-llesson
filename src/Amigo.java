35fb87c7082b25505c6237115e9563635a6cea80
Oleg <olegkuz39@gmail.com>
2022-12-29 20:29:07 +0200

    Лекция 1 JavaRush пример"Амиго" 2 пример:

---
+++ src/Amigo.java
@@ -0,0 +1,8 @@
+public class Amigo {
+    public static void main (String[]args)
+    {
+        System.out.print("Amigo");
+        System.out.print("the");
+        System.out.print("beast");
+    }
+}
