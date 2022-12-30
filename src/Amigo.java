eb260bfaee42777bdec2ffd3f383f16007622d16
Oleg <olegkuz39@gmail.com>
2022-12-30 18:20:21 +0200

    Лекция 4 лекция:

--- src/Amigo.java
+++ src/Amigo.java
@@ -1,9 +1,16 @@
 public class Amigo {
     public static void main (String[]args)
     {
-        String s = "Ami";
-        System.out.println("Amigo");
-        System.out.println("Ami"+ "go");
-        System.out.println(s + "go");
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        y = x + y;//14
+
+        x = y - x;//14-2=12
+        y = y - x;//14-12=2
+
+        System.out.println(x);
+        System.out.println(y);
     }
 }
