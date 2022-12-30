2909d104550668017974f22794863c913287d3e1
Oleg <olegkuz39@gmail.com>
2022-12-30 17:00:41 +0200

    Лекция 4 лекция:

--- src/Amigo.java
+++ src/Amigo.java
@@ -2,7 +2,6 @@ public class Amigo {
     public static void main (String[]args)
     {
         String s = "Ami";
-
         System.out.println("Amigo");
         System.out.println("Ami"+ "go");
         System.out.println(s + "go");
