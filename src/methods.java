25c586f979245cbf0a6b2660bc6647050c5c3ce5
Oleg <olegkuz39@gmail.com>
2023-01-07 18:00:28 +0200

    параметры метода

--- src/methods.java
+++ src/methods.java
@@ -1,28 +1,41 @@
 public class methods {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "Роман";
-        person1.age = 50;
+        person1.setName("Роман");
+        person1.age = 33;
+        String s1 = "Вова";
         Person person2 = new Person();
-        person2.name = "Вова";
+        person2.setName(s1);
         person2.age = 20;
         person1.speak();
         person2.speak();
-        person2.sayHello();
-
     }
 }
+
 
 class Person {
     String name;
     int age;
 
+    void setName(String userName) {
+        name = userName;
+    }
+
+    int calcLateYearsToRetiremment() {
+        int years = 65 - age;
+        return years;
+
+    }//метод
+
     void speak() {
         for (int i = 0; i < 3; i++) {
             System.out.println("Меня зовут " + name + ",мне " + age + " лет");
-        }
+
+        }//метод
     }
+
     void sayHello() {
-        System.out.println("Привет");
+        System.out.println("Привет " + name);
     }
+
 }
