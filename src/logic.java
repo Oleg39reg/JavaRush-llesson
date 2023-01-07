560d7a7306197acc549717b90b0eff70a6b74dbb
Oleg <olegkuz39@gmail.com>
2023-01-07 18:09:56 +0200

    логические операции 1

---
+++ src/logic.java
@@ -0,0 +1,13 @@
+public class logic {
+    public static void main (String[] args) {
+
+        int a = 4;
+        int b = 5;
+
+        if(a == b){
+            System.out.println("Это истина");
+        } else{
+            System.out.println("Это лож");
+        }
+    }
+}
